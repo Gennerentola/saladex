a68cdc89cef9bf845961e6566f7bc951cd89c5be
Gennerentola <gennaro.notaro1@gmail.com>
2023-07-30 01:20:37 +0200

    wip - layout grafico

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDzM2e8JkQZA1TxicC5Jf75JxXJLMBpvwM",
+
+    authDomain: "saladex-161196.firebaseapp.com",
+
+    projectId: "saladex-161196",
+
+    storageBucket: "saladex-161196.appspot.com",
+
+    messagingSenderId: "228073516449",
+
+    appId: "1:228073516449:web:17ee2ccd7c1f6bf144afe0",
+
+    measurementId: "G-M87PQV4Y9M"
+
+  }
+};
